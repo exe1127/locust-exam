a9397d281ff90dca24a4f12b84ec2635455e303e
exe1127 <exeatlas1127@gmail.com>
2022-10-13 16:55:28 -0300

    include for in program

--- test/test.py
+++ test/test.py
@@ -3,17 +3,17 @@ from locust import HttpUser, SequentialTaskSet, task, between
 headers = {
     'authority': 'btntbw73c9.execute-api.us-east-1.amazonaws.com',
     'accept': 'application/json, text/plain, */*',
-    'accept-language': 'es-419,es;q=0.9',
+    'accept-language': 'es,es-ES;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
     'authorization': 'Bearer 12345',
-    # 'origin': 'http://localhost:3000',
-    # 'referer': 'http://localhost:3000/',
-    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    'origin': 'http://react-demo-aws-starkix.s3-website-us-east-1.amazonaws.com',
+    'referer': 'http://react-demo-aws-starkix.s3-website-us-east-1.amazonaws.com/',
+    'sec-ch-ua': '"Chromium";v="106", "Microsoft Edge";v="106", "Not;A=Brand";v="99"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'cross-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.42',
 }
 
 json_data = {
@@ -32,8 +32,6 @@ headers2 = {
     'authority': 'btntbw73c9.execute-api.us-east-1.amazonaws.com',
     'accept': 'application/json, text/plain, */*',
     'accept-language': 'es-419,es;q=0.9',
-    'origin': 'http://localhost:3000',
-    'referer': 'http://localhost:3000/',
     'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
@@ -47,40 +45,42 @@ params = {
     'starId': '1',
     'userId': '1',
 }
-
-class Queue(SequentialTaskSet):
-    @task
-    def processqueue(self):
-        self.client.post("/processqueue", headers=headers, json=json_data)
+# class Queue(SequentialTaskSet):
+#     @task
+#     def advance(self):
+#         self.client.post("/processqueue", headers=headers,json={'starId': '1'})
+                         
 class Initialized(SequentialTaskSet):
+
     @task
     def enable(self):
-       self.client.post("/enableQueue", headers=headers, json=json_data)
+        self.client.post("/enableQueue", headers=headers, json={'starId': '3'})
+
     @task
     def addfan(self):
-        self.client.post('/addfan', headers=headers, json=json_data)
-    @task
-    def position(self):
-        self.client.get("/queueposition?",  params=json_data, headers=headers)
+        for id in [1,2,3,4,5]:
+            self.client.post('/addfan', headers=headers, json={
+            'starId': '3',
+            'userId': id
+        })
 
-    tasks = [Queue]
+    # @task
+    # def position(self):
+    #     self.client.get("/queueposition?",  params=json_data, headers=headers)
+
+    # tasks = [Queue]
 
 
 class Request(HttpUser):
     host = "https://btntbw73c9.execute-api.us-east-1.amazonaws.com/develop"
-   
-    tasks = [Initialized, Queue]
-    wait_time = between(1, 5)
-    
-    
 
-   
+    tasks = [Initialized]
+
+    wait_time = between(1, 5)
 
     # @task
     # def clearqueue(self):
-    #     self.client.post("/clearqueue", headers=headers, json=json_data)
-
-   
+    #     self.client.post("/clearqueue", headers=headers, json={'starId': '1',})
 
     # @task
     # def createUser(self):
